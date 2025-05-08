484504bc1a08a188cbefffbc772fd2f5bc7f72b7
Piyush Kumar <pgh5566@gmail.com>
2025-05-08 05:29:37 +0000

    Explicitly set value of attribute `SQLALCHEMY_DATABASE_URI` for each test in `test_app.py` so that they may also pass in CI/CD pipeline run

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,19 +1,22 @@
-import pytest
-from app import create_app
-from config import config
 import os
+import sys
 from unittest.mock import patch
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from config import config
+from app import create_app
 
-@patch.dict(os.environ, {"SQLALCHEMY_DATABASE_URI": "sqlite:///:memory:"})
+
 def test_app_factory_development():
+    config['development'].SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
     app = create_app(config['development'])
     assert app.config['DEBUG']
     assert app.config['SQLALCHEMY_ECHO']
     # Assuming DevelopmentConfig sets TESTING to False or it defaults to False
     assert not app.config['TESTING']
 
-@patch.dict(os.environ, {"SQLALCHEMY_DATABASE_URI": "sqlite:///:memory:"})
+
 def test_app_factory_production():
+    config['production'].SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
     app = create_app(config['production'])
     assert not app.config['DEBUG']
     assert app.config['SESSION_COOKIE_SECURE']
